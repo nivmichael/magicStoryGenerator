710becc70a351748aeffe9a9126644c389cbcc63
Michael <nivmichael@gmail.com>
2025-04-13 12:00:08 +0300

    x

--- App.tsx
+++ App.tsx
@@ -1,21 +1,20 @@
 import { useEffect } from 'react';
 import { StatusBar, SafeAreaView } from 'react-native';
-import WebViewScreen from './components/WebViewScreen';
-import PermissionService from './services/permissions';
+import WebViewScreen from './app/components/WebViewScreen';
+//import PermissionService from './app/services/permissions';
 import React from 'react';
 
 export default function App() {
-  useEffect(() => {
-    (async () => {
-      await PermissionService.requestCameraPermission();
-      await PermissionService.requestLocationPermission();
-      await PermissionService.requestMediaPermission();
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     await PermissionService.requestCameraPermission();
+  //     await PermissionService.requestLocationPermission();
+  //     await PermissionService.requestMediaPermission();
+  //   })();
+  // }, []);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar barStyle="dark-content" />
       <WebViewScreen url="https://app--magic-story-app-v2-main-740d4861.base44.app" />
     </SafeAreaView>
   );
